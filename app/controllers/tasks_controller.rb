18e10451830421819165dd5ba277b2164dc29b6f
nicole mcinerney <nicolemci@hotmail.com>
2023-08-19 21:03:16 +0100

    checklist complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
@@ -8,9 +13,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params_task)
-    @task.save
-    redirect_to tasks_path(@tasks)
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
   end
 
   def edit
@@ -19,25 +24,25 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(params_task)
-    redirect_to tasks_path(@tasks)
-  end
-
-  def show
-    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path(@tasks)
+    redirect_to tasks_path
   end
 
   private
 
-  def params_task
-    params.require(:task).permit(:name)
+  def set_task
+    @task = Task.find(params[:id])
   end
 
-
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
